bb4f4e9899d603eea2a7be13556836aa95bfba44
Islam Hany <ihhg.developer@gmail.com>
2024-04-14 01:11:38 +0200

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,12 @@
 """
 filtered_logger.py
 """
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ filter_datum """
     for field in fields:
         message = re.sub(rf'{field}=[^{separator}]+',
