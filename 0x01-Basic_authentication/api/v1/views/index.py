46b205117de99074bb996e882d7eec8bd3070235
Islam Hany <ihhg.developer@gmail.com>
2024-04-15 16:23:02 +0200

    1. Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,7 +14,7 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized", strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized """
+    abort(401)
