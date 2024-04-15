8ea9772c36f812ee0f16a416fc646653659be371
Islam Hany <ihhg.developer@gmail.com>
2024-04-15 16:29:09 +0200

    2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -28,5 +28,13 @@ def stats() -> str:
 
 @app_views.route("/unauthorized", strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/unauthorized """
+    """ GET /api/v1/unauthorized
+    """
     abort(401)
+
+
+@app_views.route("/forbidden", strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    """
+    abort(403)
