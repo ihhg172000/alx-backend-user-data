5793dc573916677a5de262c4622d44fda97b3852
Islam Hany <ihhg.developer@gmail.com>
2024-04-16 03:48:47 +0200

    refactor

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ basic_auth.py
 """
 from api.v1.auth.auth import Auth
 import base64
+import re
 
 
 class BasicAuth(Auth):
@@ -15,24 +16,21 @@ class BasicAuth(Auth):
         """
         extract_base64_authorization_header
         """
-        if type(authorization_header) is not str:
-            return None
+        if type(authorization_header) is str:
+            pattern = r"^Basic ([^\s]+)$"
+            match = re.search(pattern, authorization_header)
 
-        if not authorization_header.startswith("Basic "):
-            return None
-
-        return authorization_header[6:]
+            if match:
+                return match.group(1)
 
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
         """
         decode_base64_authorization_header
         """
-        if type(base64_authorization_header) is not str:
-            return None
-
-        try:
-            return base64.b64decode(
-                base64_authorization_header).decode("utf-8")
-        except (Exception):
-            return None
+        if type(base64_authorization_header) is str:
+            try:
+                return base64.b64decode(
+                    base64_authorization_header).decode("utf-8")
+            except (Exception):
+                pass
