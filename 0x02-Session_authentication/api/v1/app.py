dca76153356aac990667a99afaf7d5eb489ba5c2
Islam Hany <ihhg.developer@gmail.com>
2024-04-17 22:04:28 +0200

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,10 +43,12 @@ def before_request():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
 
         if auth.require_auth(request.path, execlude_paths):
-            if not auth.authorization_header(request):
+            if (not auth.authorization_header(request) and
+                    not auth.session_cookie(request)):
                 abort(401)
 
             current_user = auth.current_user(request)
