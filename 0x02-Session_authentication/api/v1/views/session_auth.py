acecf648a64c81ca1088bbb550daee7b10d375b8
Islam Hany <ihhg.developer@gmail.com>
2024-04-18 09:34:42 +0200

    8. Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,7 +3,7 @@
 session_auth.py
 """
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import abort, jsonify, request
 from models.user import User
 import os
 
@@ -46,3 +46,17 @@ def login():
     response.set_cookie(SESSION_NAME, session_id)
 
     return response
+
+
+@app_views.route("/auth_session/logout",
+                 methods=["DELETE"], strict_slashes=False)
+def logout():
+    """
+    logout
+    """
+    from api.v1.app import auth
+
+    if auth.destroy_session(request):
+        return jsonify({})
+
+    abort(404)
